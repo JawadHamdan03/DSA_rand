07ea203c9476f6ac0d919d436c54072901ee247f
JawadHamdan03 <eng.jawad121@hotmail.com>
2026-04-26 22:08:56 +0300

    find target in a tree DFS and BFS

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from collections import deque
 class Node :
     def __init__(self,val: int, right:Node=None, left:Node = None):
         self.val= val
@@ -47,10 +47,89 @@ def DFS_iterative(root):
             stack.append(curr.left)
     print(*values)
 
+def BFS_iterative(root):
+    if not root:
+        return
+
+    queue = deque()
+    queue.append(root)
+
+    while queue:
+        length = len(queue)
+        for i in range(length):
+            curr=queue.popleft()
+            print(curr.val)
+            if curr.left :
+                queue.append(curr.left)
+            if curr.right:
+                queue.append(curr.right)
+
+
+#------------------------------- Sum all nodes in Tree --------------------
+def sum_tree_DFS(root:Node, sum=0):
+    if not root :
+        return sum
+
+    sum+=root.val
+    return sum_tree_DFS(root.left,sum)+sum_tree_DFS(root.right)
+
+
+def sum_tree_BFS(root:Node):
+    if not root:
+        return
+    sum=0
+    queue = deque()
+    queue.append(root)
+
+    while queue:
+        length = len(queue)
+        curr= queue.popleft()
+        sum+=curr.val
+        if curr.left:
+            queue.append(curr.left)
+        if curr.right:
+            queue.append(curr.right)
+    return sum
+
+
+# ----------------------------- find target in a tree --------
+##DFS
+def find_target_recursive(root:Node,target:int):
+    if not root :
+        return False
+    if root.val==target:
+        return True
+    return find_target_recursive(root.left,target) or find_target_recursive(root.right,target)
+
+##BFS
+def find_target_iterative(root:Node,target:int):
+    if not root :
+        return False
+    queue= deque([root])
+
+    while queue:
+        length = len(queue)
+        curr = queue.popleft()
+        if curr.val==target :
+            return True
+        if curr.left:
+            queue.append(curr.left)
+        if curr.right:
+            queue.append(curr.right)
+
+    return False
 
 tree= Node(5)
 my_nodes=[Node(6),Node(3),Node(4),Node(8),Node(2)]
 create_BST(tree,my_nodes)
-DFS_recursive(tree)
-DFS_iterative(tree)
-
+##DFS_recursive(tree)
+##DFS_iterative(tree)
+#BFS_iterative(tree)
+#res1=sum_tree_DFS(tree)
+#res2=sum_tree_BFS(tree)
+#print(res1)
+#print(res2)
+res=find_target_recursive(tree,9)
+res2=find_target_iterative(tree,9)
+print(res)
+print(res2)
